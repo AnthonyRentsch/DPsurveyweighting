71407f6b7940d00830884814118c50ad5dfb6627
Anthony Rentsch <anthony.rentsch@gmail.com>
2019-05-07 23:38:34 -0400

    adding updated scripts to use 8 vars

--- scripts/poststratify.R
+++ scripts/poststratify.R
@@ -14,18 +14,18 @@ acs_cell_counts <- acs_cell_counts[,-1]
 cces16 <- read_tsv("data/CCES16_Common_OUTPUT_Jul2017_VV.tab", col_names = TRUE)
 
 # process CCES data
-weight_vars_cces <- c(#"gender", # sex
-                      #"birthyr", # age
+weight_vars_cces <- c("gender", # sex
+                      "birthyr", # age
                       "educ", # education
                       "race", # race
-                      #"employ", # employment status
-                      #"marstat", # marital status
+                      "employ", # employment status
+                      "marstat", # marital status
                       #"faminc", # family income
                       #"child18num", # number of children under 18
-                      #"immstat", # citizenship status
+                      "immstat", # citizenship status
                       "inputstate" # state
                       # metropolitan area
-                      ) 
+                      )
 
 cces16 <- cces16 %>%
   mutate(education = case_when(educ %in% c(1,8,9,NA) ~ 1,
@@ -38,7 +38,23 @@ cces16 <- cces16 %>%
                           race == 2 ~ 2,
                           race == 3 ~ 3,
                           race == 4 ~ 4,
-                          race %in% c(5,6,7,8,98,99,NA) ~ 5)
+                          race %in% c(5,6,7,8,98,99,NA) ~ 5),
+         sex = gender,
+         age = case_when(2016-birthyr < 18 | is.na(birthyr) ~ 1,
+                         2016-birthyr >= 18 | 2016-birthyr < 28 ~ 2,
+                         2016-birthyr >= 28 | 2016-birthyr < 38 ~ 3,
+                         2016-birthyr >= 38 | 2016-birthyr < 48 ~ 4,
+                         2016-birthyr >= 48 | 2016-birthyr < 58 ~ 5,
+                         2016-birthyr >= 58 | 2016-birthyr < 68 ~ 6,
+                         2016-birthyr >= 68 | 2016-birthyr < 78 ~ 7,
+                         2016-birthyr >= 78 | 2016-birthyr < 88 ~ 8,
+                         2016-birthyr >= 88 ~ 9),
+         employment = case_when(employ %in% c(1,2) ~ 1,
+                                employ %in% c(3,4,5,6,7,8,NA) ~ 2),
+         marital = case_when(marstat == 1 ~ 1,
+                             marstat != 1 ~ 2),
+         citizen = case_when(immstat != 2 ~ 1,
+                             immstat == 2 ~ 2)
          )
 states <- data.frame(inputstate = seq(1:56), 
                      state = tolower(c("AL","AK","","AZ","AR","CA","","CO","CT","DE","DC",
@@ -48,29 +64,23 @@ states <- data.frame(inputstate = seq(1:56),
                                        "SD","TN","TX","UT","VT","VA","","WA","WV","WI","WY")))
 cces16 <- left_join(cces16, states, by = "inputstate")
 
-cces16_slim <- cces16 %>% 
-  mutate(all_vars = paste("state", state, "race", race, "education", education, sep="_")) %>% 
-  select(all_vars, CC16_364c)
+cces16_slim <- cces16 %>% select(state, education, race, CC16_364c, CC16_301a)
   
 # process ACS data
 # rescale weights by diving by the max weight for any individual in any state
 acs_cell_counts$rescaled_n <- acs_cell_counts$n/max(state_weights$max_weight)
-
-acs_cell_counts_slim <- acs_cell_counts %>% 
-  mutate(all_vars = paste("state", state, "race", race, "education", education, sep="_")) %>% 
-  select(all_vars, rescaled_n)
-
+acs_cell_counts_slim <- acs_cell_counts %>% select(state, education, race, rescaled_n) #***
 
 # create svydesign object
 # assume SRS for illustrative purposes
 cces16.des <- svydesign(ids = ~ 1, data = cces16_slim)
 cces16.des.ps <- postStratify(design = cces16.des,
-                              strata = ~all_vars,
+                              strata = ~state+race+education,
                               population = acs_cell_counts_slim,
                               partial = TRUE)
 
-# 
-voteshare.weighted <- as.data.frame(svytable(~CC16_364c, design=cces16.des.ps)) %>% 
+# weighted to true ACS
+voteshare.weighted <- as.data.frame(svytable(~CC16_364c+race, design=cces16.des.ps)) %>% 
   rename(preference=CC16_364c, share=Freq) %>% 
   mutate(preference = case_when(preference == 1 ~ "Trump",
                                 preference == 2 ~ "Clinton",
@@ -80,12 +90,18 @@ voteshare.weighted <- as.data.frame(svytable(~CC16_364c, design=cces16.des.ps)) 
                                 preference == 6 ~ "Won't vote",
                                 preference == 7 ~ "Not sure",
                                 preference %in% c(8,9, NA) ~ "Skipped/not asked"),
-         share = share/sum(share))
-voteshare.weighted
+         race = case_when(race == 1 ~ "White",
+                          race == 2 ~ "Black",
+                          race == 3 ~ "Hispanic",
+                          race == 4 ~ "Asian",
+                          race == 5 ~ "Other")) %>% 
+  group_by(race) %>% mutate(share = share/sum(share))
+voteshare.weighted %>% View()
 
-# unweighted counts
-unweighted.res <- cces16 %>% rename(preference=CC16_364c) %>% 
-  group_by(preference) %>% summarise(n = n()) %>% 
+# unweighted
+cces16.unweighted.des <- svydesign(ids = ~ 1, data = cces16_slim)
+unweighted.res <- as.data.frame(svytable(~CC16_364c+race, design=cces16.unweighted.des)) %>% 
+  rename(preference=CC16_364c, share=Freq) %>% 
   mutate(preference = case_when(preference == 1 ~ "Trump",
                                 preference == 2 ~ "Clinton",
                                 preference == 3 ~ "Johnson",
@@ -94,15 +110,57 @@ unweighted.res <- cces16 %>% rename(preference=CC16_364c) %>%
                                 preference == 6 ~ "Won't vote",
                                 preference == 7 ~ "Not sure",
                                 preference %in% c(8,9, NA) ~ "Skipped/not asked"),
-         share = n/sum(n)) %>% select(preference, share) 
-unweighted.res
+         race = case_when(race == 1 ~ "White",
+                          race == 2 ~ "Black",
+                          race == 3 ~ "Hispanic",
+                          race == 4 ~ "Asian",
+                          race == 5 ~ "Other")) %>% 
+  group_by(race) %>% mutate(share = share/sum(share))
+unweighted.res %>% View()
+
+# weighted to noisy ACS
+epsilon <- 0.5
+scale <- max(state_weights$max_weight)/epsilon
+noisy_acs <- acs_cell_counts_slim
+noisy_acs$noisy_n <- noisy_acs$rescaled_n + rlap(mu=0, b=scale, size=nrow(noisy_acs))
+noisy_acs$noisy_n <- ifelse(noisy_acs$noisy_n < 0, 1, noisy_acs$noisy_n)
+noisy_acs_slim <- noisy_acs %>% select(all_vars, noisy_n)
+
+cces16.noisy.des <- svydesign(ids = ~ 1, data = cces16_slim)
+cces16.noisy.des.ps <- postStratify(design = cces16.des,
+                              strata = ~all_vars,
+                              population = noisy_acs_slim,
+                              partial = TRUE)
+
+voteshare.noisy.weighted <- as.data.frame(svytable(~CC16_364c+race, design=cces16.noisy.des.ps)) %>% 
+  rename(preference=CC16_364c, share=Freq) %>% 
+  mutate(preference = case_when(preference == 1 ~ "Trump",
+                                preference == 2 ~ "Clinton",
+                                preference == 3 ~ "Johnson",
+                                preference == 4 ~ "Stein",
+                                preference == 5 ~ "Other",
+                                preference == 6 ~ "Won't vote",
+                                preference == 7 ~ "Not sure",
+                                preference %in% c(8,9, NA) ~ "Skipped/not asked"),
+         race = case_when(race == 1 ~ "White",
+                          race == 2 ~ "Black",
+                          race == 3 ~ "Hispanic",
+                          race == 4 ~ "Asian",
+                          race == 5 ~ "Other")) %>% 
+  group_by(race) %>% mutate(share = share/sum(share))
+voteshare.noisy.weighted %>% View()
+
+
+# combine to compare private versus non-private release
+combined <- merge(voteshare.weighted, voteshare.noisy.weighted, 
+                  by=c("preference", "race"), suffixes=c("_true", "_noisy"))
 
 
 # Appendix
 # compare to actual CCES weights
 # cces.16.actual.des <- svydesign(ids = ~1, data = cces16, weights = ~commonweight)
-# cces16.actual.svytable <- as.data.frame(svytable(~CC16_364c, design=cces.16.actual.des)) %>% 
-#   rename(preference=CC16_364c, share=Freq) %>% 
+# cces16.actual.svytable <- as.data.frame(svytable(~CC16_364c, design=cces.16.actual.des)) %>%
+#   rename(preference=CC16_364c, share=Freq) %>%
 #   mutate(preference = case_when(preference == 1 ~ "Trump",
 #                                 preference == 2 ~ "Clinton",
 #                                 preference == 3 ~ "Johnson",
--- scripts/pull_acs_data.R
+++ scripts/pull_acs_data.R
@@ -8,17 +8,17 @@
 rm(list = ls())
 require(plyr); require(dplyr)
 setwd("~/Desktop/Harvard/S19/cs208/DPsurveyweighting/data/")
-cell_counts_file_name = "cell_counts_3var.csv"
+cell_counts_file_name = "cell_counts_8var.csv"
 state_weights_file_name = "state_weights.csv"
 
 states <- tolower(c(state.abb, "DC"))
 person_vars <- c("ST",
-                 #"AGEP",
-                 #"CIT",
-                 #"MAR",
+                 "AGEP",
+                 "CIT",
+                 "MAR",
                  "SCHL",
-                 #"SEX",
-                 #"ESR",
+                 "SEX",
+                 "ESR",
                  "RAC1P"
                  #"PAOC",
                  #"PINCP"
@@ -50,27 +50,43 @@ state_weights <- matrix(NA, nrow=length(states), ncol=3)
 i <- 1
 start_time <- Sys.time()
 
-for(state in states) {
+for(state in states[1]) {
   cat("State:", state)
   # grab ACS data for a state
   state_person_data <- get_acs_data(state)
   # pre-process
   state_person_data <- state_person_data %>% 
-    mutate(education = case_when(SCHL %in% c(seq(1,15,by=1), NA) ~ 1,
+    mutate(age = case_when(AGEP >=0 | AGEP < 18 ~ 1,
+                            AGEP >= 18 | AGEP < 28 ~ 2,
+                            AGEP >= 28 | AGEP < 38 ~ 3,
+                            AGEP >= 38 | AGEP < 48 ~ 4,
+                            AGEP >= 48 | AGEP < 58 ~ 5,
+                            AGEP >= 58 | AGEP < 68 ~ 6,
+                            AGEP >= 68 | AGEP < 78 ~ 7,
+                            AGEP >= 78 | AGEP < 88 ~ 8,
+                            AGEP >= 88 ~ 9),
+           citizen = case_when(CIT %in% c(1,2,3,4) ~ 1,
+                           CIT == 5 ~ 2),
+           marital = case_when(MAR == 1 ~ 1,
+                           MAR != 1 ~ 2),
+           education = case_when(SCHL %in% c(seq(1,15,by=1), NA) ~ 1,
                             SCHL %in% c(16,17) ~ 2,
                             SCHL %in% c(18,19) ~ 3,
                             SCHL == 20 ~ 4,
                             SCHL == 21 ~ 5,
                             SCHL %in% c(22,23,24) ~ 6),
+           employment = case_when(ESR %in% c(1,2,4,5) ~ 1,
+                           ESR %in% c(NA,3,6) ~ 2),
            race = case_when(RAC1P == 1 & HISP == 1 ~ 1,
-                            RAC1P == 2 & HISP == 1 ~ 2,
-                            HISP != 1 ~ 3,
-                            RAC1P == 6 & HISP == 1 ~ 4,
-                            RAC1P %in% c(3,4,5,7,8,9) & HISP == 1 ~ 5)
-           )
+                             RAC1P == 2 & HISP == 1 ~ 2,
+                             HISP != 1 ~ 3,
+                             RAC1P == 6 & HISP == 1 ~ 4,
+                             RAC1P %in% c(3,4,5,7,8,9) & HISP == 1 ~ 5)
+           ) %>% rename(sex=SEX)
   # get cell counts 
   state_cell_counts <- get_weighted_cell_counts(state_person_data, weight_var=person_weight,
-                                                "ST","education","race")
+                                                "ST","age","citizen","marital","education",
+                                                "employment","race","SEX")
   # append state abbrevation
   state_cell_counts$state <- state
   # add df to list of dfs
@@ -115,29 +131,7 @@ write.csv(state_weights_df, file=state_weights_file_name)
 
 ### variable processing for 8 variables
 ### will have to update to reflect CCES vars
-# mutate(AGEP = case_when(AGEP >=0 | AGEP < 18 ~ 1, 
-#                         AGEP >= 18 | AGEP < 35 ~ 2,
-#                         AGEP >= 35 | AGEP < 65 ~ 3,
-#                         AGEP >= 65 ~ 4),
-#        CIT = case_when(CIT %in% c(1,2,3,4) ~ 1,
-#                        CIT == 5 ~ 2),
-#        MAR = case_when(MAR == 1 ~ 1,
-#                        MAR != 1 ~ 2),
-#        SCHL = case_when(SCHL %in% c(seq(1,15,by=1), NA) ~ 1,
-#                         SCHL %in% c(16,17,18,19) ~ 2,
-#                         SCHL == 20 ~ 3,
-#                         SCHL == 21 ~ 4,
-#                         SCHL %in% c(22,23,24) ~ 5),
-#        ESR = case_when(ESR %in% c(1,2,4,5) ~ 1,
-#                        ESR %in% c(NA,3,6) ~ 2),
-#        RAC1P = case_when(RAC1P == 1 ~ 1,
-#                          RAC1P == 2 ~ 2,
-#                          RAC1P == 3 ~ 3,
-#                          RAC1P == 4 ~ 4,
-#                          RAC1P == 6 ~ 6,
-#                          RAC1P == 7 ~ 7,
-#                          RAC1P %in% c(5,8,9) ~ 5,
-#                          HISP != 1 ~ 999))
+
 
 
 
